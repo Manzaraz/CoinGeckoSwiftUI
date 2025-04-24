4014b7371b220fe66a9c0a1d29422f0da1727b32
Christian Manzaraz <magnusmanz@icloud.com>
2025-04-24 20:59:15 -0300

    Initial Commit

---
+++ CoinGeckoSwiftUI/CoinGeckoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinGeckoSwiftUIApp.swift
+//  CoinGeckoSwiftUI
+//
+//  Created by Christian Manzaraz on 24/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct CoinGeckoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
